697c6a5c0931100d76343d403731386dbd8e4b9b
Simon Andercats <simontettmar@proton.me>
2025-12-16 21:05:18 +0000

    Update draw.js

--- Example2/javascript/draw.js
+++ Example2/javascript/draw.js
@@ -4,7 +4,7 @@ License
 
 (The MIT License)
 
-Copyright (c) 2025 Simon T.T.
+Copyright (c) 2025 Simon Andercats
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), 
 to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
